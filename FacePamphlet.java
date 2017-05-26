b99808520985aeada37eee43f86aad5d6f2a5198
Greg Dardis <dardis.greg@gmail.com>
2017-05-26 10:12:44 -0600

    added interactors to North and West, they don't do anything yet

--- FacePamphlet.java
+++ FacePamphlet.java
@@ -11,8 +11,22 @@ import acm.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class FacePamphlet extends Program 
+public class FacePamphlet extends ConsoleProgram 
 					implements FacePamphletConstants {
+	
+	/* Instance variables for North Controller */
+	private JTextField nameField;
+	private JButton addButton;
+	private JButton deleteButton;
+	private JButton lookupButton;
+	
+	/* Instance variables for West Controller */
+	private JTextField changeStatusField;
+	private JButton changeStatusButton;
+	private JTextField changePictureField;
+	private JButton changePictureButton;
+	private JTextField addFriendField;
+	private JButton addFriendButton;
 
 	/**
 	 * This method has the responsibility for initializing the 
@@ -20,8 +34,61 @@ public class FacePamphlet extends Program
 	 * initialization that needs to be performed.
 	 */
 	public void init() {
-		// You fill this in
+		createNorthController();
+		createWestController();
+		addActionListeners();
     }
+	
+	private void createNorthController() {
+		addLabelAndTextField();
+		addThreeButtonsNorth();
+	}
+	
+	private void addLabelAndTextField() {
+		add(new JLabel("Name"), NORTH);
+		nameField = new JTextField(TEXT_FIELD_SIZE);
+		nameField.addActionListener(this);
+		add(nameField, NORTH);
+	}
+	
+	private void addThreeButtonsNorth() {
+		addButton = new JButton("Add");
+		deleteButton = new JButton("Delete");
+		lookupButton = new JButton("Lookup");
+		add(addButton, NORTH);
+		add(deleteButton, NORTH);
+		add(lookupButton, NORTH);
+	}
+	
+	private void createWestController() {
+		addChangeStatusInteractors();
+		addChangePictureInteractors();
+		addAddFriendInteractors();
+	}
+	
+	private void addChangeStatusInteractors() {
+		changeStatusField = new JTextField(TEXT_FIELD_SIZE);
+		changeStatusField.addActionListener(this);
+		changeStatusButton = new JButton("Change Status");
+		add(changeStatusField, WEST);
+		add(changeStatusButton, WEST);
+	}
+	
+	private void addChangePictureInteractors() {
+		changePictureField = new JTextField(TEXT_FIELD_SIZE);
+		changePictureField.addActionListener(this);
+		changePictureButton = new JButton("Change Picture");
+		add(changePictureField, WEST);
+		add(changePictureButton, WEST);
+	}
+	
+	private void addAddFriendInteractors() {
+		addFriendField = new JTextField(TEXT_FIELD_SIZE);
+		addFriendField.addActionListener(this);
+		addFriendButton = new JButton("Add Friend");
+		add(addFriendField, WEST);
+		add(addFriendButton, WEST);
+	}
     
   
     /**
